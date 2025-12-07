11eedccdf2f13b19be48c25a50a4301cfac8a3b7
Luke Perron <LukeDPerron@users.noreply.github.com>
2025-12-07 23:24:31 +0000

    added new endpoint

--- presidio-anonymizer/app.py
+++ presidio-anonymizer/app.py
@@ -7,6 +7,7 @@ from pathlib import Path
 
 from flask import Flask, Response, jsonify, request
 from presidio_anonymizer import AnonymizerEngine, DeanonymizeEngine
+from presidio_anonymizer.entities import InvalidParamError, OperatorConfig
 from presidio_anonymizer.entities import InvalidParamError
 from presidio_anonymizer.services.app_entities_convertor import AppEntitiesConvertor
 from werkzeug.exceptions import BadRequest, HTTPException
@@ -94,6 +95,30 @@ class Server:
             return Response(
                 deanonymized_response.to_json(), mimetype="application/json"
             )
+        
+        @self.app.route("/genz", methods=["GET"])
+        def genz() -> Response:
+            content = request.get_json()
+            if not content:
+                raise BadRequest("Invalid request json")
+
+            text = content.get("text", "")
+
+            genz_config = {
+                "DEFAULT": OperatorConfig(operator_name="genz", params={})
+            }
+
+            analyzer_results = AppEntitiesConvertor.analyzer_results_from_json(
+                content.get("analyzer_results")
+            )
+
+            anonymized = self.anonymizer.anonymize(
+                text=text,
+                analyzer_results=analyzer_results,
+                operators=genz_config,
+            )
+
+            return Response(anonymized.to_json(), mimetype="application/json")
 
         @self.app.route("/anonymizers", methods=["GET"])
         def anonymizers():
